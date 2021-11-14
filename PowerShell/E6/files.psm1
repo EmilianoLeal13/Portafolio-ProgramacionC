6b8cf29a8a869a9342ee328e9e806096e1190add
EmilianoLeal13 <90713649+EmilianoLeal13@users.noreply.github.com>
2021-11-13 19:23:52 -0600

    Add files via upload

---
+++ PowerShell/E6/files.psm1
@@ -0,0 +1,80 @@
+function Get-CompressFiles
+  {
+     param(
+    [Parameter()][String]$Ruta,
+    [Parameter()][String]$DesPath)
+
+    $Ruta = Read-Host "Ingrese la ruta del archivo a comprimir"
+    $DesPath = Read-Host "Ingrese la ruta donde sera guardado"
+    try
+    {
+        Compress-Archive -LiteralPath $Ruta -DestinationPath $DesPath -InvalidArgument Stop
+    }
+    catch { "Argumento invalido, Alguna ruta fue mal escrita, favor de escribirlo correctamente" }
+}
+ function Get-ModifyFiles
+{
+    param(
+    [Parameter()][String]$op,
+    [Parameter()][String]$Ruta,
+    [Parameter()][String]$RutaDes)
+
+    $op = Read-Host "
+    Selecciona que quieres hacer:
+    1: Crear Un Archivo
+    2: Borrar Un Archivo
+    3: Copiar Un Archivo
+    :" 
+
+    switch ( $op )
+    {
+
+        1{
+            $Ruta = Read-Host "Ruta del archivo a crear"
+            New-Item -Path $Ruta -ItemType File
+        }
+
+        2{
+            $Ruta = Read-Host "Ruta del archivo a borrar"
+            Remove-Item -Path $Ruta
+        }
+
+        3{
+            $Ruta = Read-Host "Ruta del archivo a copiar"
+            $RutaDes = Read-Host "Ruta del archivo donde se pegara"
+            Copy-Item -Path $Ruta -Destination $RutaDes
+
+        }
+    }
+} 
+function Get-Unzip
+{
+     param(
+    [Parameter()][String]$Ruta,
+    [Parameter()][String]$DesPath)
+
+    $Ruta = Read-Host "Ingrese la ruta del archivo a comprimir"
+    $DesPath = Read-Host "Ingrese la ruta donde sera guardado"
+    try
+    {
+        Expand-Archive -LiteralPath $Ruta -DestinationPath $DesPath -InvalidOperation Stop
+    }
+    catch { "Operacion invalida, el archivo a decompriir ya existe" }
+}
+function Get-ArchivosPorFecha
+{
+    param(
+    [Parameter()][Array]$TipoArchivo,
+    [Parameter()][String]$Mes,
+    [Parameter()][Array]$Año,
+    [Parameter()][Array]$Ruta)
+
+    Read-Host "Tipo de Archivo" $TipoArchivo
+    Read-Host "Mes" $Mes
+    Read-host "Año" $Año
+    Read-Host "Ruta" $Ruta
+
+    Get-ChildItem -Path $Ruta -Include $TipoArchivo -Recurse 
+
+    Where-Object {$.lastwritetime.month -eq $Mes -AND $.lastwritetime.year -eq $Año }
+}
---
+++ PowerShell/E6/main.ps1
@@ -0,0 +1,36 @@
+﻿"""
+Emiliano Leal Campos
+E-manuel Manzanarez Mora
+Fuentes:
+https://uanledu.sharepoint.com/sites/Section_02305010133602031420000101204062/_layouts/15/Doc.aspx?sourcedoc={c1753fb2-fa72-4b85-ac8d-3f9167b11a0b}&action=view&wd=target%28_Content%20Library%2F1.%20Shell%20y%20scripting.one%7C8a98ad7c-d9a9-4e34-a924-eeb22076d04c%2FAn%C3%A1lisis%20de%20m%C3%B3dulos%7C0cf9a18e-767d-4f2c-808a-a825d9d182f5%2F%29
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/compress-archive?view=powershell-7.1
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/expand-archive?view=powershell-7.1&viewFallbackFrom=powershell-7.1https%3A%2F%2Fdocs.microsoft.com%2Fen-us%2Fpowershell%2Fmodule%2Fmicrosoft.powershell.archive%2Fexpand-archive%3Fview%3Dpowershell-7.1
+"""
+
+do
+{
+
+  Write-Host "Menu"
+  Write-Host "1 = Comprimir Archivos"
+  Write-Host "2 = Descomprimir Archivos"
+  Write-Host "3 = Crear, Borrar o Copiar"
+  Write-Host "4 = Obtener Archivos Por Fecha"
+  Write-Host "0 = Salir"
+  $op = Read-Host
+  switch($op)
+
+  {
+    1 {Write-Host "Get-CompressFiles"
+    Get-CompressFiles}
+
+    2 {Write-Host "Get-Unzip"
+    Get-Unzip}
+    
+    3 {Write-Host "Get-ModifyFiles"
+    Get-ModifyFiles}
+
+    4 {Write-Host "Get-ArchivosPorFecha"
+    Get-ArchivosPorFecha}
+
+  }
+  }while($op -ne 0)
